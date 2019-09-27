3dd1f342724e893c4071081c9b02893ad95ef773
Máté Odler <odlermate@gmail.com>
2019-09-27 13:09:34 +0200

    félkész/ 2. feladat közepéig kész

--- AukcioProjekt/Program.cs
+++ AukcioProjekt/Program.cs
@@ -17,6 +17,49 @@ namespace AukcioProjekt
             a.Kiir();
 
 
+
+            var festmenyek = new List<Festmeny>();
+            festmenyek.Add(new Festmeny("Napsütés", "Anna", "barokk"));
+            festmenyek.Add(new Festmeny("Búzamező", "András", "barokk"));
+
+            Console.WriteLine("Kérlek add meg hány festményt szeretnél hozzáadni");
+            int db = Convert.ToInt32(Console.ReadLine());
+            string nev;
+            string cime;
+            string stilusa;
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine("Ki írta a festményt?");
+                nev = Console.ReadLine();
+                Console.WriteLine("Mi a festmény címe?");
+                cime = Console.ReadLine();
+                Console.WriteLine("Milyen stilusban keszult a festmeny?");
+                stilusa = Console.ReadLine();
+                festmenyek.Add(new Festmeny(nev, cime, stilusa));
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.Read();
         }
     }
